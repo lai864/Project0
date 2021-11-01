953e33df152c81c9d0ca070ae26a41cdb58c7f52
chen <3291450631@qq.com>
2021-11-01 21:50:10 +0800

    scened idea use git

--- src/MyGit/Git_test.java
+++ src/MyGit/Git_test.java
@@ -3,5 +3,9 @@ package MyGit;
 public class Git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+
+
     }
 }
